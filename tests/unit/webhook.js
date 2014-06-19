fecbf4ea94b1d9be8e1b879f756b2839b3351751
Paweł Ledwoń <me@ludvik.pl>
2014-06-19 13:30:26 +0200

    Implement basic WebHook validation

--- tests/unit/webhook.js
+++ tests/unit/webhook.js
@@ -6,6 +6,61 @@ var WebHook = require("../../lib/webhook");
 describe("WebHook", function() {
   var pusher;
 
+  beforeEach(function() {
+    pusher = new Pusher({
+      appId: 10000,
+      key: "123456789",
+      secret: "beef"
+    });
+  });
+
+  describe("#isValid", function() {
+    it("should return true for a webhook with correct signature", function() {
+      var webhook = new WebHook({
+        headers: {
+          "x-pusher-key": "123456789",
+          "x-pusher-signature": "df1465f5ff93f83238152fd002cb904f9562d39569e68f00a6bfa0d8ccf88334",
+          "content-type": "application/json"
+        },
+        rawBody: JSON.stringify({
+          time_ms: 1403175510755,
+          events: [{ channel: "test_channel", name: "channel_vacated" }]
+        })
+      }, pusher);
+      expect(webhook.isValid()).to.be(true);
+    });
+
+    it("should return false for a webhook with incorrect key", function() {
+      var webhook = new WebHook({
+        headers: {
+          "x-pusher-key": "000",
+          "x-pusher-signature": "df1465f5ff93f83238152fd002cb904f9562d39569e68f00a6bfa0d8ccf88334",
+          "content-type": "application/json"
+        },
+        rawBody: JSON.stringify({
+          time_ms: 1403175510755,
+          events: [{ channel: "test_channel", name: "channel_vacated" }]
+        })
+      }, pusher);
+      expect(webhook.isValid()).to.be(false);
+    });
+
+    it("should return false for a webhook with incorrect signature", function() {
+      var webhook = new WebHook({
+        headers: {
+          "x-pusher-key": "123456789",
+          "x-pusher-signature": "000",
+          "content-type": "application/json"
+        },
+        rawBody: JSON.stringify({
+          time_ms: 1403175510755,
+          events: [{ channel: "test_channel", name: "channel_vacated" }]
+        })
+      }, pusher);
+      expect(webhook.isValid()).to.be(false);
+    });
+  });
+
   describe("#getData", function() {
     it("should return a parsed JSON body", function() {
       var webhook = new WebHook({

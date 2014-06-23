4b0c65dad65c4c9764ca91f635103af23314d1b3
Paweł Ledwoń <me@ludvik.pl>
2014-06-23 13:53:58 +0200

    Handle request errors more consistently

--- tests/unit/pusher/get.js
+++ tests/unit/pusher/get.js
@@ -58,6 +58,30 @@ describe("Pusher", function() {
       });
     });
 
+    it("should call back with a RequestError if Pusher responds with 4xx", function(done) {
+      var mock = nock("http://api.pusherapp.com")
+        .filteringPath(function(path) {
+          return path
+            .replace(/auth_timestamp=[0-9]+/, "auth_timestamp=X")
+            .replace(/auth_signature=[0-9a-f]{64}/, "auth_signature=Y");
+        })
+        .get(
+          "/apps/999/test?auth_key=111111&auth_timestamp=X&auth_version=1.0&auth_signature=Y"
+        )
+        .reply(400, "Error");
+
+      pusher.get({ path: "/test", params: {} }, function(error, request, response) {
+        expect(error).to.be.a(Pusher.RequestError);
+        expect(error.message).to.equal("Unexpected status code 400");
+        expect(error.url).to.match(
+          /^http:\/\/api.pusherapp.com\/apps\/999\/test\?auth_key=111111&auth_timestamp=[0-9]+&auth_version=1\.0&auth_signature=[a-f0-9]+$/
+        );
+        expect(error.statusCode).to.equal(400);
+        expect(error.body).to.equal("Error");
+        done();
+      });
+    });
+
     it("should respect the scheme, host and port config", function(done) {
       var pusher = new Pusher({
         appId: 999,
@@ -103,7 +127,15 @@ describe("Pusher", function() {
       pusher.get({ path: "/test", params: {} }, function(error, request, response) {
         var expectedError = new Error("ETIMEDOUT");
         expectedError.code = "ETIMEDOUT";
-        expect(error).to.eql(expectedError);
+
+        expect(error).to.be.a(Pusher.RequestError);
+        expect(error.message).to.equal("Request failed with an error");
+        expect(error.error).to.eql(expectedError);
+        expect(error.url).to.match(
+          /^http:\/\/api.pusherapp.com\/apps\/999\/test\?auth_key=111111&auth_timestamp=[0-9]+&auth_version=1\.0&auth_signature=[a-f0-9]+$/
+        );
+        expect(error.statusCode).to.equal(null);
+        expect(error.body).to.equal(null);
         done();
       });
     });

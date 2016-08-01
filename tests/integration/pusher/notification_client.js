43550ec22a3b465b915b199e1fe73c71119e06b5
James Fisher <jameshfisher@gmail.com>
2016-08-01 17:09:00 +0100

    WIP add a test for reporting name and version, but can't figure out how to use this nock thing

--- tests/integration/pusher/notification_client.js
+++ tests/integration/pusher/notification_client.js
@@ -3,13 +3,13 @@ var NotificationClient = require("../../../lib/notification_client");
 var nock = require('nock');
 
 describe("NativeNotificationClient", function() {
-	var client;
+  var client;
 
-	beforeEach(function(){
-		client = new NotificationClient({ appId: 1234, key: "f00d", secret: "beef" });
-		nock.cleanAll();
-		nock.disableNetConnect();
-	});
+  beforeEach(function(){
+    client = new NotificationClient({ appId: 1234, key: "f00d", secret: "beef" });
+    nock.cleanAll();
+    nock.disableNetConnect();
+  });
 
   afterEach(function() {
     nock.cleanAll();
@@ -17,165 +17,195 @@ describe("NativeNotificationClient", function() {
   });
 
   xit("should send in the success case", function(done){
-  	var mock = nock("nativepush-cluster1.pusher.com:80")
-		client.notify(['yolo'],{
-			'apns': {
-				'aps': {
-					'alert':{
-						'title': 'yolo',
-						'body': 'woot'
-					}
-				}
-			},
-			'gcm': {
-				'notification': {
-					'title': 'huzzah',
-					'icon': 'woot'
-				}
-			}
+    var mock = nock("nativepush-cluster1.pusher.com:80")
+    client.notify(['yolo'],{
+      'apns': {
+        'aps': {
+          'alert':{
+            'title': 'yolo',
+            'body': 'woot'
+          }
+        }
+      },
+      'gcm': {
+        'notification': {
+          'title': 'huzzah',
+          'icon': 'woot'
+        }
+      }
 
-		}, function(){
-			expect(mock.isDone()).to.be(true);
-			done();
-		});
+    }, function(){
+      expect(mock.isDone()).to.be(true);
+      done();
+    });
   });
 
-	it("should remove restricted GCM keys", function(){
-		var notification = {
-			'gcm': {
-				'to': 'woot',
-				'registration_ids': ['woot', 'bla'],
-				'notification': {
-					'title': 'yipee',
-					'icon': 'huh'
-				}
-			}
-		};
+  // TODO enable this test if you can figure out how to use this nock thing.
+  // I just can't figure it out.
+  // https://github.com/node-nock/nock/issues/640
+  xit("should have X-Pusher-Library header", function(done){
+    var headers;
 
-		client.validateNotification(notification);
-		expect(notification).to.eql({ gcm: { notification: { title: 'yipee', icon: 'huh' }}});
-	});
+    var mock = nock("http://nativepush-cluster1.pusher.com:80")
+      .post("/server_api/v1/apps/1234/notifications")
+      .reply(function(uri, requestBody) {
+        console.log("REPLYING");
+        headers = this.req.headers;
+      });
 
-	it("should validate that either apns or gcm are present", function(){
-		var notification = {};
-		expect(function(){
-			client.validateNotification(notification);
-		}).to.throwException(/Notification must have fields APNS or GCM/);
-	});
+    client.notify(['yolo'],{
+      'apns': {
+        'aps': {
+          'alert': {
+            'title': 'yolo',
+            'body': 'woot'
+          }
+        }
+      }
+    }, function(){
+      console.log("DONE");
+      expect(headers).to.have.key('X-Pusher-Library');
+      expect(mock.isDone()).to.be(true);
+      done();
+    });
+  });
 
-	it("should validate that only one interest is sent to", function(){
-		expect(function(){
-			client.notify(['yolo', 'woot'], {});
-		}).to.throwException(/Currently sending to more than one interest is unsupported/);
-	})
+  it("should remove restricted GCM keys", function(){
+    var notification = {
+      'gcm': {
+        'to': 'woot',
+        'registration_ids': ['woot', 'bla'],
+        'notification': {
+          'title': 'yipee',
+          'icon': 'huh'
+        }
+      }
+    };
 
-	it("should invalidate certain gcm payloads", function(){
-		var invalidGcmPayloads = [
-			{
-				'gcm': {
-					'time_to_live': -1,
-					'notification': {
-						'title': 'yipee',
-						'icon': 'huh'
-					}
-				},
-				exception: /GCM time_to_live must be between 0 and 241920 \(4 weeks\)/
-			},
-			{
-				'gcm': {
-					'time_to_live': 241921,
-					'notification': {
-						'title': 'yipee',
-						'icon': 'huh'
-					}
-				},
-				exception: /GCM time_to_live must be between 0 and 241920 \(4 weeks\)/
-			},
-			{
-				'gcm': {
-					'notification': {
-						'title': 'yipee',
-					}
-				},
-				exception: /GCM icon must be a string and not empty/
-			},
-			{
-				'gcm': {
-					'notification': {
-						'icon': 'huh'
-					}
-				},
-				exception: /GCM title must be a string and not empty/
-			},
-			{
-				'gcm': {
-					'notification': {
-						'title': '',
-						'icon': 'huh'
-					}
-				},
-				exception: /GCM title must be a string and not empty/
-			},
-			{
-				'gcm': {
-					'notification': {
-						'title': 'yipee',
-						'icon': ''
-					}
-				},
-				exception: /GCM icon must be a string and not empty/
-			}
-		]
+    client.validateNotification(notification);
+    expect(notification).to.eql({ gcm: { notification: { title: 'yipee', icon: 'huh' }}});
+  });
 
-		for (var index in invalidGcmPayloads) {
-			var invalidPayload = invalidGcmPayloads[index];
-			expect(function(){
-				client.notify(['yolo'], invalidPayload);
-			}).to.throwException(invalidPayload.exception || /%/);
-		}
-	});
+  it("should validate that either apns or gcm are present", function(){
+    var notification = {};
+    expect(function(){
+      client.validateNotification(notification);
+    }).to.throwException(/Notification must have fields APNS or GCM/);
+  });
 
-	it("validates webhook config", function(){
-		invalidWebhookConfig = [
-			{
-				'webhook_level': 'DEBUG',
-				'apns': {
-					'alert': {
-					'title': 'yolo',
-					'body': 'woot'
-					}
-				},
-				'gcm': {
-					'notification': {
-						'title': 'yipee',
-						'icon': 'huh'
-					}
-				},
-				exception: 'webhook_level cannot be used without a webhook_url'
-			},
-			{
-				'webhook_level': 'FOOBAR',
-				'webhook_url': 'http://webhook.com',
-				'apns': {
-					'alert': {
-					'title': 'yolo',
-					'body': 'woot'
-					}
-				},
-				'gcm': {
-					'notification': {
-						'title': 'yipee',
-						'icon': 'huh'
-					}
-				},
-				exception: 'webhook_level must be either INFO or DEBUG. Blank will default to INFO'
-			}];
+  it("should validate that only one interest is sent to", function(){
+    expect(function(){
+      client.notify(['yolo', 'woot'], {});
+    }).to.throwException(/Currently sending to more than one interest is unsupported/);
+  })
 
-		for (var index in invalidWebhookConfig) {
-			var invalidPayload = invalidWebhookConfig[index];
-			expect(function(){
-				client.notify(['yolo'], invalidPayload);
-			}).to.throwException(new RegExp(invalidPayload.exception));
-		}
-	})
+  it("should invalidate certain gcm payloads", function(){
+    var invalidGcmPayloads = [
+      {
+        'gcm': {
+          'time_to_live': -1,
+          'notification': {
+            'title': 'yipee',
+            'icon': 'huh'
+          }
+        },
+        exception: /GCM time_to_live must be between 0 and 241920 \(4 weeks\)/
+      },
+      {
+        'gcm': {
+          'time_to_live': 241921,
+          'notification': {
+            'title': 'yipee',
+            'icon': 'huh'
+          }
+        },
+        exception: /GCM time_to_live must be between 0 and 241920 \(4 weeks\)/
+      },
+      {
+        'gcm': {
+          'notification': {
+            'title': 'yipee',
+          }
+        },
+        exception: /GCM icon must be a string and not empty/
+      },
+      {
+        'gcm': {
+          'notification': {
+            'icon': 'huh'
+          }
+        },
+        exception: /GCM title must be a string and not empty/
+      },
+      {
+        'gcm': {
+          'notification': {
+            'title': '',
+            'icon': 'huh'
+          }
+        },
+        exception: /GCM title must be a string and not empty/
+      },
+      {
+        'gcm': {
+          'notification': {
+            'title': 'yipee',
+            'icon': ''
+          }
+        },
+        exception: /GCM icon must be a string and not empty/
+      }
+    ]
+
+    for (var index in invalidGcmPayloads) {
+      var invalidPayload = invalidGcmPayloads[index];
+      expect(function(){
+        client.notify(['yolo'], invalidPayload);
+      }).to.throwException(invalidPayload.exception || /%/);
+    }
+  });
+
+  it("validates webhook config", function(){
+    invalidWebhookConfig = [
+      {
+        'webhook_level': 'DEBUG',
+        'apns': {
+          'alert': {
+          'title': 'yolo',
+          'body': 'woot'
+          }
+        },
+        'gcm': {
+          'notification': {
+            'title': 'yipee',
+            'icon': 'huh'
+          }
+        },
+        exception: 'webhook_level cannot be used without a webhook_url'
+      },
+      {
+        'webhook_level': 'FOOBAR',
+        'webhook_url': 'http://webhook.com',
+        'apns': {
+          'alert': {
+          'title': 'yolo',
+          'body': 'woot'
+          }
+        },
+        'gcm': {
+          'notification': {
+            'title': 'yipee',
+            'icon': 'huh'
+          }
+        },
+        exception: 'webhook_level must be either INFO or DEBUG. Blank will default to INFO'
+      }];
+
+    for (var index in invalidWebhookConfig) {
+      var invalidPayload = invalidWebhookConfig[index];
+      expect(function(){
+        client.notify(['yolo'], invalidPayload);
+      }).to.throwException(new RegExp(invalidPayload.exception));
+    }
+  })
 })

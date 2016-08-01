f471fda951ecb65aac2bc6364553040325144cb2
James Fisher <jameshfisher@gmail.com>
2016-08-01 15:42:20 +0100

    enable disabled test which seems to pass locally

--- tests/integration/pusher/notification_client.js
+++ tests/integration/pusher/notification_client.js
@@ -16,7 +16,7 @@ describe("NativeNotificationClient", function() {
     nock.enableNetConnect();
   });
 
-  xit("should send in the success case", function(done){
+  it("should send in the success case", function(done){
   	var mock = nock("nativepush-cluster1.pusher.com:80")
 		client.notify(['yolo'],{
 			'apns': {
